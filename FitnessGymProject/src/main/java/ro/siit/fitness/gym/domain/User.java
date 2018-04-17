ccb10e1c88e31771f5bf6f668af3b08695d8c2fb
andreea-PC\andreea <andreeapece@yahoo.com>
2018-04-17 20:54:35 +0300

    inclusion of validations and client log in

---
+++ FitnessGymProject/src/main/java/ro/siit/fitness/gym/domain/User.java
@@ -0,0 +1,26 @@
+package ro.siit.fitness.gym.domain;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class User {
+    List<String> roles = new LinkedList<>();
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
